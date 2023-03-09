5b8057fcf7a010cfa7996420162f5a424d6fc5cc
devedzic <devedzic@gmail.com>
2023-03-09 02:15:47 +0100

    Spotify tracks

--- my_spotify.py
+++ my_spotify.py
@@ -1,4 +1,4 @@
-"""Functions for Spotify authentication.
+"""Functions for Spotify authentication, getting tracks from a playlist, etc.
 """
 
 
@@ -26,4 +26,19 @@ def get_spotify_object(env_file_path: str) -> spotipy.client.Spotify:
     auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     return spotipy.Spotify(auth_manager=auth_manager)
 
+
+#%%
+def get_all_tracks_from_playlist(playlist_id: str, env_file_path: str) -> list:
+    spot = get_spotify_object(env_file_path)
+    offset = 0
+    all_tracks = []
+    tracks = spot.playlist_items(playlist_id, offset=offset)           # the first 100 tracks (offset = 0)
+    if not tracks['items']:                                             # if the playlist is empty
+        return []
+    all_tracks.extend(tracks['items'])
+    while tracks['next']:
+        offset += 100
+        tracks = spot.playlist_items(playlist_id, offset=offset)       # the next 100 tracks
+        all_tracks.extend(tracks['items'])
+    return all_tracks
 
